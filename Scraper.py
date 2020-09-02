f5eda117bf81fb4d7fa6175340e04e378cf77908
tt-dd-tt <61581135+tt-dd-tt@users.noreply.github.com>
2020-09-02 14:17:54 +0200

    Update Scraper.py
    Passive version, email will be written in case "Nemecko" will disappear from the website.

--- Scraper.py
+++ Scraper.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[93]:
-
-
 #! python3
 import bs4, requests, smtplib, re, mailshake
 
@@ -14,30 +10,20 @@ getPage.raise_for_status() #check ci je chyba
 soup = bs4.BeautifulSoup(getPage.text,'html.parser') #toto vrati linky
 zoznam = soup.findAll('div',attrs={'class':'journal-content-article'});
 
-
-# In[94]:
-
-
 zoznam2 = ' '.join(map(str, zoznam)) #skonvertovanie na list
 def check():
     if ('Nemecko') in zoznam2:
         return(True)
 
-
-if check() == True:
+if check() == False:
     conn = smtplib.SMTP('smtp.gmail.com', 587)
     conn.ehlo() # call this to start the connection
     conn.starttls() # starts tls encryption. When we send our password it will be encrypted.
     conn.login('libor.riska@gmail.com', 'lfgmhcbwgraoqzdx')
-    conn.sendmail('libor.riska@gmail.com', 'libor.riska@gmail.com', 'Subject:Slovensko stale povazuje Nemecko za bezpecnu krajinu')
+    conn.sendmail('libor.riska@gmail.com', 'libor.riska@gmail.com', 'Subject:Slovensko NEPOVAZUJE Nemecko za bezpecnu krajinu')
     conn.quit()
     
-      
-    
 
-
-
-# In[ ]:
 
 
 
